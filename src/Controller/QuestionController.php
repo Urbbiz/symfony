b2f33fa87004463da56795c59b5f73716b0d80d8
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-10-09 08:32:26 +0300

    add some twig files

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,6 +21,15 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf('future page to show question :%s" !',ucwords(str_replace('-', ' ',$slug)) ));
+
+        $answers = [
+            'Make sure your cat is sitting perrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+        return $this->render('question/show.html.twig',[
+            'question'=> ucwords(str_replace('-', ' ',$slug)),
+            'answers' => $answers]);
+//        return new Response(sprintf('future page to show question :%s" !',ucwords(str_replace('-', ' ',$slug)) ));
     }
 }
