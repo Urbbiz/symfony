e49522e27ab88936218fa7ddc79f47720ee8dd29
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-10-18 13:18:42 +0300

    services

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use App\Service\MarkdownHelper;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
+
+
+    private $logger;
+    private $isDebug;
+
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
     /**
      * @Route("/", name="app_homepage")
      */
@@ -28,15 +39,21 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode!');
+        }
+
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
         $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
-        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+
+            $parsedQuestionText = $markdownHelper->parse($questionText);
+//        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'questionText' => $parsedQuestionText,
